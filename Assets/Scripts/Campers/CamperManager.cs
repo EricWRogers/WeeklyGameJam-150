bf4def59dc8bd46df61e28cd955531c9778274c9
Azee <aziztitu1996@gmail.com>
2020-05-23 01:11:45 -0500

    Campers WIP

--- Assets/Scripts/Campers/CamperManager.cs
+++ Assets/Scripts/Campers/CamperManager.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CamperManager : MonoBehaviour
 {
+    public GameObject camperPrefab;
+    public Transform hidingSpotsContainer;
+
+    public int campersCount = 6;
+
+    private Randomizer<HidingSpot> hidingSpotsRandomizer;
+
+    void Awake()
+    {
+        hidingSpotsRandomizer = new Randomizer<HidingSpot>(hidingSpotsContainer.GetComponentsInChildren<HidingSpot>());
+        SpawnCampers();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +28,13 @@ public class CamperManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void SpawnCampers()
+    {
+        for (int i = 0; i < campersCount; i++)
+        {
+            
+        }
     }
 }
