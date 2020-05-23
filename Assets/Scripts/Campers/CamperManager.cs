6d3675d0befb81d8f65ca8009be9a20060a8de58
Azee <aziztitu1996@gmail.com>
2020-05-23 01:21:59 -0500

    Implements spawning of campers in random hiding spots

--- Assets/Scripts/Campers/CamperManager.cs
+++ Assets/Scripts/Campers/CamperManager.cs
@@ -10,6 +10,7 @@ public class CamperManager : MonoBehaviour
 
     public int campersCount = 6;
 
+    [HideInInspector] public List<Camper> campers = new List<Camper>();
     private Randomizer<HidingSpot> hidingSpotsRandomizer;
 
     void Awake()
@@ -21,20 +22,22 @@ public class CamperManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     void SpawnCampers()
     {
         for (int i = 0; i < campersCount; i++)
         {
-            
+            var hidingSpot = hidingSpotsRandomizer.GetRandomItem();
+            var camper = Instantiate(camperPrefab, hidingSpot.transform.position, hidingSpot.transform.rotation)
+                .GetComponent<Camper>();
+
+            campers.Add(camper);
         }
     }
 }
