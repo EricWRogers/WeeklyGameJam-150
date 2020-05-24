129cca22d095726e0ecf26bf89462f66c91fd99f
Azee <aziztitu1996@gmail.com>
2020-05-23 21:23:25 -0500

    Implements vision system for NPC Campers

--- Assets/Scripts/Campers/Camper.cs
+++ Assets/Scripts/Campers/Camper.cs
@@ -6,12 +6,22 @@ using UnityEngine.AI;
 
 public class Camper : MonoBehaviour
 {
+    [Header("References")] public GameObject avatar;
+
+    [Header("Stats")]
+    public float maxVisionRange = 30;
+    public float maxVisionAngle = 45;
+    public Transform visionRoot => avatar.transform;
+
     [Header("Hiding Params")]
     public RangeFloat maxHideDurationRange;
 
     [Header("Dev Tools")]
     [SerializeField] [Button("Move to new hiding spot", "MoveToNewHidingSpot")]
     private bool _btnMoveToNewHidingSpot;
+
+    [Header("Logs")] [SerializeField]
+    [ReadOnly] private bool _canSeePlayer;
 
     private NavMeshAgent navMeshAgent;
 
@@ -28,6 +38,12 @@ public class Camper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _canSeePlayer = CanSeePlayer();
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        DebugExtension.DebugCone(visionRoot.position, visionRoot.forward * maxVisionRange, maxVisionAngle);
     }
 
     public void MoveToNewHidingSpot()
@@ -40,5 +56,29 @@ public class Camper : MonoBehaviour
     HidingSpot FindNewHidingSpot()
     {
         return CamperManager.Instance.hidingSpotsRandomizer.GetRandomItem();
+    }
+
+    public bool CanSeePlayer()
+    {
+        var playerLoc = LevelManager.Instance.playerLocation;
+
+        var toPlayer = playerLoc.position - visionRoot.position;
+
+        if (toPlayer.magnitude <= maxVisionRange)
+        {
+            var angleToPlayer = Vector3.Angle(visionRoot.forward, toPlayer.normalized);
+            if (angleToPlayer <= maxVisionAngle)
+            {
+                if (Physics.Raycast(visionRoot.position, toPlayer.normalized, out var hit, maxVisionRange))
+                {
+                    if (hit.transform.CompareTag("Player"))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
