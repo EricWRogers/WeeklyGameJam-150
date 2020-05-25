4055878110cf33e319cd4b37b76af72cbd09cb7b
William Lacalle <digitalbros7@gmail.com>
2020-05-24 22:51:45 -0500

    Basic Camera and third person implementation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,6 +7,10 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     CharacterController charController;
+    
+    public GameObject cinemachine;
+    public CinemachineBrain cinemachineBrain;
+    CinemachineFreeLook freeLookCam;
 
     Vector2 movementInput;
 
@@ -13,8 +18,10 @@ public class PlayerMovement : MonoBehaviour
 
     void Awake()
     {
-
         charController = GetComponent<CharacterController>();
+        freeLookCam = cinemachine.GetComponent<CinemachineFreeLook>();
+
+        HelperUtilities.UpdateCursorLock(true);
     }
 
     void OnMovement(InputValue inputValue)
@@ -22,13 +29,24 @@ public class PlayerMovement : MonoBehaviour
         movementInput = inputValue.Get<Vector2>();
     }
 
+    Vector3 OrientPlayer(Vector3 baseVector)
+    {
+        Vector3 forwardDir = cinemachineBrain.gameObject.transform.forward * baseVector.z;
+        forwardDir.y = 0;
+
+        Vector3 rightDir = cinemachineBrain.gameObject.transform.right * baseVector.x;
+        rightDir.y = 0;
+
+        return (forwardDir + rightDir).normalized;
+    }
+
     void Update()
     {
         float h = movementInput.x;
         float v = movementInput.y;
         Vector3 movementVector = new Vector3(h, 0, v);
+        movementVector = OrientPlayer(movementVector);
 
         charController.Move(movementVector * speed * Time.deltaTime);
-
     }
 }
