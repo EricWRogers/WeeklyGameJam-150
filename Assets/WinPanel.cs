77ba5d15261d93418d5dbfc1cdd6d25f669a9961
EricWRogers <yerick147083@gmail.com>
2020-05-28 02:23:43 -0500

    Win lose

---
+++ Assets/WinPanel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WinPanel : MonoBehaviour
+{
+    void OnEnable()
+    {
+        GetComponent<TMP_Text>().text = "You Monster you ate " + PlayerModel.Instance.campersEaten + " campers!";
+    }
+}
